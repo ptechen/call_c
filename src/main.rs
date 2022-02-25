8dbbe7b4f663c1e0e4abd14b1cbdab5d06ff34c6
taochen <taochen@yunjumai.com>
2022-02-26 07:22:02 +0800

    update

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use libc::size_t;
+
+#[link(name="snappy")]
+extern {
+    fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+}
+
+fn main() {
+    let x = unsafe {
+        snappy_max_compressed_length(100)
+    };
+    println!("Hello, world! {}", x);
+}
+
