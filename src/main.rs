c3dca37d60ca92426ac9b0ea5f46a2579b253fb9
taochen <taochen@yunjumai.com>
2022-02-26 15:40:24 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
+// use libc::size_t;
+//
+// #[link(name="snappy")]
+// extern {
+//     fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+// }
+//
+// fn main() {
+//     let x = unsafe {
+//         snappy_max_compressed_length(100)
+//     };
+//     println!("Hello, world! {}", x);
+// }
+
 use libc::size_t;
 
-#[link(name="snappy")]
+#[link(name="pcre2")]
 extern {
-    fn snappy_max_compressed_length(source_length: size_t) -> size_t;
 }
 
 fn main() {
-    let x = unsafe {
-        snappy_max_compressed_length(100)
-    };
-    println!("Hello, world! {}", x);
+    println!("Hello, world!");
 }
 
