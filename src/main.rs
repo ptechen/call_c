29cc1d978f9e2a0c9e67cc286c1be1e0ecd7c69b
taochen <taochen@yunjumai.com>
2022-03-01 11:15:54 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // use libc::size_t;
-//
+
 // #[link(name="snappy")]
 // extern {
 //     fn snappy_max_compressed_length(source_length: size_t) -> size_t;
@@ -12,9 +12,18 @@
 //     println!("Hello, world! {}", x);
 // }
 
+
+
 use libc::size_t;
 
-#[link(name="pcre")]
+// #[link(name="pcre")]
+// extern {
+// }
+
+/// export LIBRARY_PATH="$LIBRARY_PATH:/opt/homebrew/Cellar/pcre2/10.39/lib"
+/// 编译安装也无法找到pcre2
+/// 目前无法找到pcre2
+#[link(name="pcre2")]
 extern {
 }
 
